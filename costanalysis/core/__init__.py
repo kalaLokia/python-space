3433e748f557cb0471af5d8f3eb1bacb6a101a11
kalaLokia <kalalokia@gmail.com>
2021-07-15 19:27:16 +0530

    minor changes

--- costanalysis/core/__init__.py
+++ costanalysis/core/__init__.py
@@ -4,18 +4,17 @@ BOM_DATA_DIR = "data/Bom Hierarchy final.csv"
 ITEM_DATA_DIR = "data/materials.csv"
 ARTICLE_RATES_DIR = "data/articles.csv"
 
-ROYALTY = 0.5
-SELLING_DISTRIBUTION = 0.1675
+ROYALTY = 0.4
+SELLING_DISTRIBUTION = 0.1275
 SALES_RETURN = 0.01
-SELL_DISTR_ROYALTY = 0.1725  # Selling and Distribution = 16.75%, Royalty = 0.50%
 
 FIXED_RATES = {
-    "wastage_and_benefits": 9.72,
-    "salaries_and_emoluments": 0.79,
-    "other_factory_overheads": 1.73,
-    "admin_expenses": 1.37,
-    "interest_and_bank_charges": 0.02,
-    "depreciation": 4.35,
-    "other_expenses": 10.0,
-    "finance_costs": 1.06,
+    "wastage_and_benefits": 0.1,
+    "salaries_and_emoluments": 0.1,
+    "other_factory_overheads": 0.1,
+    "admin_expenses": 0.1,
+    "interest_and_bank_charges": 0.1,
+    "depreciation": 0.1,
+    "other_expenses": 0.1,
+    "finance_costs": 0.1,
 }  # EXPENSES_AND_OVERHEADS
